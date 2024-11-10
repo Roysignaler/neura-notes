3bcb4773db7955e601da1b601ef927e903b40d45
Roysignaler <roysignaler@gmail.com>
2024-11-10 22:33:13 +0100

    Adding editor

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import Editor from "./Editor";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -30,7 +31,7 @@ function Document({ id }: { id: string }) {
     }
   };
   return (
-    <div>
+    <div className="flex-1 h-full bg-white p-5">
       <div className="flex max-w-6xl mx-auto justify-between pb-5">
         <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
           {/* Update title */}
@@ -50,6 +51,7 @@ function Document({ id }: { id: string }) {
         {/* Avatars */}
       </div>
       {/* Collaborative Editor */}
+      <Editor />
     </div>
   );
 }
