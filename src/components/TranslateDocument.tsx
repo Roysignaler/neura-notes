bdf997549f935c0d9950b82909463b7c6280dcfd
Roysignaler <roysignaler@gmail.com>
2024-11-11 11:47:15 +0100

    Making translate document pt. 1

--- src/components/TranslateDocument.tsx
+++ src/components/TranslateDocument.tsx
@@ -1,6 +1,34 @@
+"use client";
 import React from "react";
 
-function TranslateDocument(doc: any) {
+import * as Y from "yjs";
+
+type Language =
+  | "english"
+  | "spanish"
+  | "portuguese"
+  | "french"
+  | "german"
+  | "chinese"
+  | "arabic"
+  | "hindi"
+  | "russian"
+  | "japanese";
+
+const languages: Language[] = [
+  "english",
+  "spanish",
+  "portuguese",
+  "french",
+  "german",
+  "chinese",
+  "arabic",
+  "hindi",
+  "russian",
+  "japanese",
+];
+
+function TranslateDocument({ doc }: { doc: Y.Doc }) {
   return <div>TranslateDocument</div>;
 }
 
