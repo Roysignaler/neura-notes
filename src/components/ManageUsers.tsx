c554999bf9293eed76ef61a60876dbfbdecfa6d9
Roysignaler <roysignaler@gmail.com>
2024-11-11 10:05:12 +0100

    Manage users with delete server action

---
+++ src/components/ManageUsers.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { FormEvent, useState, useTransition } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { useUser } from "@clerk/nextjs";
+import { useRoom } from "@liveblocks/react/suspense";
+import useOwner from "@/lib/useOwner";
+import { collectionGroup, query, where } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../firebase";
+import { toast } from "sonner";
+import { removeUserFromDocument } from "../../actions/actions";
+
+function ManageUsers() {
+  const { user } = useUser();
+  const room = useRoom();
+  const isOwner = useOwner();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+
+  const [usersInRoom] = useCollection(
+    user && query(collectionGroup(db, "rooms"), where("roomId", "==", room.id))
+  );
+
+  const handleDelete = (userId: string) => {
+    startTransition(async () => {
+      if (!user) return;
+
+      const { success } = await removeUserFromDocument(room.id, userId);
+
+      if (success) {
+        toast.success("User removed from room succesfully!");
+      } else {
+        toast.error("Failed to remove user from room!");
+      }
+    });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <Button asChild variant="outline">
+        <DialogTrigger>Users ({usersInRoom?.docs.length})</DialogTrigger>
+      </Button>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Invite a User to collaborate</DialogTitle>
+          <DialogDescription>
+            Enter the email of the user you want to invite.
+          </DialogDescription>
+        </DialogHeader>
+
+        <hr className="my-2" />
+        <div className="flex flex-col space-y-2">
+          {usersInRoom?.docs.map((doc) => (
+            <div
+              key={doc.data().userId}
+              className="flex items-center justify-between"
+            >
+              <p className="font-light">
+                {doc.data().userId === user?.emailAddresses[0].toString()
+                  ? `You (${doc.data().userId})`
+                  : doc.data().userId}
+              </p>
+              <div className="flex items-center gap-2">
+                {isOwner &&
+                  doc.data().userId !== user?.emailAddresses[0].toString() && (
+                    <Button
+                      variant="destructive"
+                      onClick={() => handleDelete(doc.data().userId)}
+                      disabled={isPending}
+                      size="sm"
+                    >
+                      {isPending ? "Removing.." : "X"}
+                    </Button>
+                  )}
+                <Button variant="outline">{doc.data().role}</Button>
+              </div>{" "}
+            </div>
+          ))}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ManageUsers;
