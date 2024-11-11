021fb953d67c6cb2aec4657bd7ca05bdb8119056
Roysignaler <roysignaler@gmail.com>
2024-11-11 14:03:14 +0100

    Functional translation feature

--- src/components/TranslateDocument.tsx
+++ src/components/TranslateDocument.tsx
@@ -1,11 +1,33 @@
 "use client";
-import React from "react";
+import React, { useTransition, useState, FormEvent } from "react";
 
 import * as Y from "yjs";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTrigger,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "./ui/dialog";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+import { BotIcon } from "lucide-react";
+import Markdown from "react-markdown";
 
 type Language =
   | "english"
   | "spanish"
+  | "danish"
   | "portuguese"
   | "french"
   | "german"
@@ -18,6 +40,7 @@ type Language =
 const languages: Language[] = [
   "english",
   "spanish",
+  "danish",
   "portuguese",
   "french",
   "german",
@@ -29,7 +52,93 @@ const languages: Language[] = [
 ];
 
 function TranslateDocument({ doc }: { doc: Y.Doc }) {
-  return <div>TranslateDocument</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [language, setLanguage] = useState<string>("");
+  const [summary, setSummary] = useState("");
+  const [question, setQuestion] = useState("");
+  const [isPending, startTransition] = useTransition();
+
+  const handleAskQuestion = (e: FormEvent) => {
+    e.preventDefault();
+
+    startTransition(async () => {
+      const documentData = doc.get("document-store").toJSON();
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/translateDocument`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            documentData,
+            targetLang: language,
+          }),
+        }
+      );
+
+      if (res.ok) {
+        const { translated_text } = await res.json();
+        setSummary(translated_text);
+        toast.success("Translated Summary succesfully!");
+      }
+    });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <Button asChild variant="outline">
+        <DialogTrigger>Translate</DialogTrigger>
+      </Button>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Translate the Document</DialogTitle>
+          <DialogDescription>
+            <Select>
+              Select a Language and AI will translate a summary of the document
+              in the selected language.
+            </Select>
+          </DialogDescription>
+
+          <hr className="mt-5" />
+          {question && <p className="mt-5 text-gray-500">Q: {question}</p>}
+        </DialogHeader>
+
+        {summary && (
+          <div className="flex flex-col items-start max-h-96 overflow-y-scroll gap-2 p-5 bg-gray-100">
+            <div className="flex">
+              <BotIcon className="w-10 flex-shrink-0" />
+              <p>GPT {isPending ? "is thinking.." : "Says:"}</p>
+            </div>
+            <p>{isPending ? "Thinking.." : <Markdown>{summary}</Markdown>}</p>
+          </div>
+        )}
+
+        <form className="flex gap-2" onSubmit={handleAskQuestion}>
+          <Select
+            value={language}
+            onValueChange={(value) => setLanguage(value)}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a Language" />
+            </SelectTrigger>
+            <SelectContent>
+              {languages.map((language) => (
+                <SelectItem key={language} value={language}>
+                  {language.charAt(0).toUpperCase() + language.slice(1)}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <Button type="submit" disabled={!language || isPending}>
+            {isPending ? "Translating.." : "Translate"}
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default TranslateDocument;
