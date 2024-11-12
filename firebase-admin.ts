3f39941e43df463f51c31062726065eee1bd4227
Roysignaler <roysignaler@gmail.com>
2024-11-12 10:06:06 +0100

    Change to firebase-admin

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -8,7 +8,9 @@ import {
 
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("./service_key.json");
+const serviceKey = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+    );
 
 let app: App;
 
