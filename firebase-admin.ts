e0f32023e1e20f15e68bfae654028ea16904a7ca
Roysignaler <roysignaler@gmail.com>
2024-11-12 01:57:35 +0100

    changes to firebase configurations

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -5,20 +5,33 @@ import {
     getApp,
     cert
 } from "firebase-admin/app";
-
 import { getFirestore } from "firebase-admin/firestore";
-
-const serviceKey = require("./service_key.json");
 
 let app: App;
 
-if (getApps().length === 0) {
-    app = initializeApp({credential: cert(serviceKey),
-    }); 
-} else { 
-    app = getApp() ;
-} 
+// Define service account credentials using environment variables
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
 
-const adminDb = getFirestore(app); 
+};
+
+if (getApps().length === 0) {
+    app = initializeApp({
+        credential: cert(serviceAccount),
+    });
+} else {
+    app = getApp();
+}
+
+const adminDb = getFirestore(app);
 
 export { app as adminApp, adminDb };
